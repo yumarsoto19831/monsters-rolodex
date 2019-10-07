b66e92a3cf95b25b2c5f46a230999e7df9f1b466
Yumar Sotolongo Rivero <ysotolongo@centricsoftware.com>
2019-10-06 22:40:24 -0400

    Updates

---
+++ precache-manifest.92274e9f54a3814100d8a232aa0005fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a8adb121adecc5c687542936b931fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d20cddfcbdb950cc4f84",
+    "url": "/monsters-rolodex/static/css/main.8cf2beba.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monsters-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "d20cddfcbdb950cc4f84",
+    "url": "/monsters-rolodex/static/js/main.06eb20f7.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
